7789aa5b39695ec14818f233fc78ee727eec968a
otto15 <koscheibismark@yandex.ru>
2024-04-04 00:10:28 +0300

    graphql sample

--- core-api/src/main/kotlin/com/skufbet/core/api/resolver/EventResolver.kt
+++ core-api/src/main/kotlin/com/skufbet/core/api/resolver/EventResolver.kt
@@ -1,4 +1,14 @@
 package com.skufbet.core.api.resolver
 
+import com.skufbet.core.api.domain.Event
+import org.springframework.graphql.data.method.annotation.QueryMapping
+import org.springframework.stereotype.Controller
+import java.util.UUID
+
+@Controller
 class EventResolver {
+    @QueryMapping("events")
+    fun getEvents(): List<Event> {
+        return listOf(Event(UUID.randomUUID(), "csgo"), Event(UUID.randomUUID(), "puk"))
+    }
 }
