5d76e47b1ccde1c8832abcf8b9dd849a155fe7ea
Dmitri <dmitribazilov@gmail.com>
2024-04-06 02:03:07 +0300

    batched queries simplified bd

--- core-api/src/main/kotlin/com/skufbet/core/api/graphql/model/event/Event.kt
+++
@@ -1,3 +0,0 @@
-package com.skufbet.core.api.graphql.model.event
-
-data class Event(val id: Int, val tournamentId: Int, val sportId: Int, val name: String, val isEnd: Boolean)
--- core-api/src/main/kotlin/com/skufbet/core/api/graphql/model/event/Tournament.kt
+++
@@ -1,3 +0,0 @@
-package com.skufbet.core.api.graphql.model.event
-
-data class Tournament(val id: Int, val name: String, val isEnd: Boolean)
