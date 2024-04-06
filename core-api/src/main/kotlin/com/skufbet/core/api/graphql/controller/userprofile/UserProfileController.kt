27e43100ad9ce23c4b13d8d6929159315617ad83
otto15 <koscheibismark@yandex.ru>
2024-04-06 03:36:08 +0300

    fix

--- core-api/src/main/kotlin/com/skufbet/core/api/graphql/controller/userprofile/UserProfileController.kt
+++ core-api/src/main/kotlin/com/skufbet/core/api/graphql/controller/userprofile/UserProfileController.kt
@@ -6,12 +6,10 @@ import com.skufbet.core.api.graphql.model.userprofile.mutation.payload.UserProfi
 import com.skufbet.core.api.userprofile.domain.UserProfile
 import com.skufbet.core.api.userprofile.service.UserProfileCreationService
 import com.skufbet.core.api.userprofile.service.command.UserProfileCreateCommand
-import jakarta.validation.Valid
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.MutationMapping
 import org.springframework.graphql.data.method.annotation.SchemaMapping
 import org.springframework.stereotype.Controller
-import org.springframework.validation.annotation.Validated
 
 @Controller
 class UserProfileController(
