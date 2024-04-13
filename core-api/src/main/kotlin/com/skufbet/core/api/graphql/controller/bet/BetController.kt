97e71126521353fc9cbab4248f280bab11ce110a
Dmitri <dimibazi123@gmail.com>
2024-04-13 15:00:12 +0300

    add polling endpoint

--- core-api/src/main/kotlin/com/skufbet/core/api/graphql/controller/bet/BetController.kt
+++ core-api/src/main/kotlin/com/skufbet/core/api/graphql/controller/bet/BetController.kt
@@ -1,10 +1,10 @@
 package com.skufbet.core.api.graphql.controller.bet
 
-import com.skufbet.core.api.clients.userprofile.UserProfileApiClient
 import com.skufbet.core.api.bet.dto.GetUserProfileTo
 import com.skufbet.core.api.bet.dto.UpdateUserBalanceRequestTo
 import com.skufbet.core.api.bet.service.BetService
 import com.skufbet.core.api.bet.service.command.BetCreateCommand
+import com.skufbet.core.api.client.userprofile.UserProfileApiClient
 import com.skufbet.core.api.graphql.model.bet.mutation.BetMutation
 import com.skufbet.core.api.graphql.model.bet.mutation.input.BetCreateInput
 import com.skufbet.core.api.graphql.model.bet.mutation.payload.BetCreatePayload
@@ -12,6 +12,7 @@ import com.skufbet.core.api.graphql.model.content.Bet
 import com.skufbet.core.api.graphql.model.content.BetStatus
 import org.springframework.graphql.data.method.annotation.Argument
 import org.springframework.graphql.data.method.annotation.MutationMapping
+import org.springframework.graphql.data.method.annotation.QueryMapping
 import org.springframework.graphql.data.method.annotation.SchemaMapping
 import org.springframework.stereotype.Controller
 import java.lang.RuntimeException
@@ -51,6 +52,11 @@ class BetController(
             TimeUnit.SECONDS
         )
         return BetCreatePayload(bet.id)
+    }
+
+    @QueryMapping
+    fun betById(@Argument betId: Int): Bet? {
+        return betService.get(betId)
     }
 
     fun BetCreateInput.toCommand() = BetCreateCommand(
